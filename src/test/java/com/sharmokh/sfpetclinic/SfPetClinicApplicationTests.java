2b94d9e6486de7c8c84112d7632aa13eed5cbfe1
Steve <steve.sharmokh@gmail.com>
2019-06-25 17:48:40 -0700

    Initial Project Setup

---
+++ src/test/java/com/sharmokh/sfpetclinic/SfPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sharmokh.sfpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
