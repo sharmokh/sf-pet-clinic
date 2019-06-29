f64806ed99b50aef8b22fd4acb7b72786b63f385
Steve <steve.sharmokh@gmail.com>
2019-06-29 14:51:25 -0600

    Fixed failing pet-clinic-web Test during packaging.

--- pet-clinic-web/src/test/java/sfpetclinic/SfPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfpetclinic/SfPetClinicApplicationTests.java
@@ -1,11 +1,11 @@
-package sfpetclinic;
+package com.sharmokh.sfpetclinic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-// @RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SfPetClinicApplicationTests {
 
