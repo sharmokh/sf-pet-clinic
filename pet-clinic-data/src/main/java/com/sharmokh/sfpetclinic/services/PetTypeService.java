9f27c5bfbe6e8bdef689e5e05fe557a23608d11f
Steve <steve.sharmokh@gmail.com>
2019-07-08 10:20:58 -0700

    Added PetTypeService. Closes #29.

---
+++ pet-clinic-data/src/main/java/com/sharmokh/sfpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.sharmokh.sfpetclinic.services;
+
+import com.sharmokh.sfpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/sharmokh/sfpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package com.sharmokh.sfpetclinic.services.map;
+
+import com.sharmokh.sfpetclinic.model.PetType;
+import com.sharmokh.sfpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findByID(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+
+}
