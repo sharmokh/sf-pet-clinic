8f71bb46c73a943270dc605ec2845de5a7cc2c67
Steve <steve.sharmokh@gmail.com>
2019-07-08 10:09:44 -0700

    Added Speciality and added to Vet.  Closes #27.

---
+++ pet-clinic-data/src/main/java/com/sharmokh/sfpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.sharmokh.sfpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
