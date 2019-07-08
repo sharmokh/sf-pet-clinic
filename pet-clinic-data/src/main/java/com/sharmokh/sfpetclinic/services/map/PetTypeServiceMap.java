ed5e3c13ae9d7d20e323d63b12f2fb92dfbada98
Steve <steve.sharmokh@gmail.com>
2019-07-08 10:31:54 -0700

    Added PetTypes to bootstrap dataloader. Closes #32.

--- pet-clinic-data/src/main/java/com/sharmokh/sfpetclinic/services/map/PetTypeServiceMap.java
+++ pet-clinic-data/src/main/java/com/sharmokh/sfpetclinic/services/map/PetTypeServiceMap.java
@@ -2,10 +2,12 @@ package com.sharmokh.sfpetclinic.services.map;
 
 import com.sharmokh.sfpetclinic.model.PetType;
 import com.sharmokh.sfpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
-public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
 
     @Override
     public Set<PetType> findAll() {
--- pet-clinic-web/src/main/java/com/sharmokh/sfpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/sharmokh/sfpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.sharmokh.sfpetclinic.bootstrap;
 
 import com.sharmokh.sfpetclinic.model.Owner;
+import com.sharmokh.sfpetclinic.model.PetType;
 import com.sharmokh.sfpetclinic.model.Vet;
 import com.sharmokh.sfpetclinic.services.OwnerService;
+import com.sharmokh.sfpetclinic.services.PetTypeService;
 import com.sharmokh.sfpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Zoe");
