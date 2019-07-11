5f01cc8dfdaf241684a0375661bccb3fe11186b9
Steve <steve.sharmokh@gmail.com>
2019-07-11 13:34:18 -0700

    Added CRUD test for Owner Map Service.  Implemented findByLastName.  Updated POMs.  Closes #51.

---
+++ pet-clinic-data/src/test/java/com/sharmokh/sfpetclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,65 @@
+package com.sharmokh.sfpetclinic.services.map;
+
+import com.sharmokh.sfpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class OwnerMapServiceTest {
+
+    OwnerMapService ownerMapService;
+    final String firstName = "Steve";
+    final String lastName = "Sharmokh";
+    Long ownerID;
+
+
+    @BeforeEach
+    void setUp() {
+        Owner owner = Owner.builder().firstName(firstName).lastName(lastName).build();
+        ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+        ownerMapService.save(owner);
+        ownerID = owner.getId();
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> ownerSet = ownerMapService.findAll();
+        assertEquals(1, ownerSet.size());
+    }
+
+    @Test
+    void findByID() {
+        Owner owner = ownerMapService.findByID(ownerID);
+        assertEquals(ownerID, owner.getId());
+    }
+
+    @Test
+    void save() {
+        Owner owner = ownerMapService.save(Owner.builder().firstName("Donovan").build());
+        assertNotNull(owner);
+        assertNotNull(owner.getId());
+    }
+
+    @Test
+    void delete() {
+        Owner owner = ownerMapService.findByID(ownerID);
+        ownerMapService.delete(owner);
+        assertEquals(0, ownerMapService.findAll().size());
+    }
+
+    @Test
+    void deleteByID() {
+        ownerMapService.deleteByID(ownerID);
+        assertEquals(0, ownerMapService.findAll().size());
+    }
+
+    @Test
+    void findByLastName() {
+        Owner owner = ownerMapService.findByLastName(lastName);
+        assertEquals(lastName, owner.getLastName());
+    }
+}
