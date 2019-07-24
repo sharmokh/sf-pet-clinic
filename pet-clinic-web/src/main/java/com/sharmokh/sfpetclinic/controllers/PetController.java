366751af59019789ae39fb5d7176493505cc3f16
Steve <steve.sharmokh@gmail.com>
2019-07-24 14:36:34 -0700

    Added Pet Controller Model Attributes.  #56

---
+++ pet-clinic-web/src/main/java/com/sharmokh/sfpetclinic/controllers/PetController.java
@@ -0,0 +1,47 @@
+package com.sharmokh.sfpetclinic.controllers;
+
+import com.sharmokh.sfpetclinic.model.Owner;
+import com.sharmokh.sfpetclinic.model.PetType;
+import com.sharmokh.sfpetclinic.services.OwnerService;
+import com.sharmokh.sfpetclinic.services.PetService;
+import com.sharmokh.sfpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{id}")
+public class PetController {
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long id) {
+        return ownerService.findByID(id);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+
+}
---
+++ pet-clinic-web/src/test/java/com/sharmokh/sfpetclinic/controllers/PetControllerTest.java
@@ -0,0 +1,20 @@
+package com.sharmokh.sfpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PetControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void populatePetTypes() {
+    }
+
+    @Test
+    void findOwner() {
+    }
+
+}
